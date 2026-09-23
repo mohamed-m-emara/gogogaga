790b4e691399ee6385cae58881fe25a24e4b4b01
mohamed-m-emara <mohamed.mohsen1701@gmail.com>
2026-09-23 22:15:57 +0300

    added the area of the circle

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static double calculateArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+    
+}
